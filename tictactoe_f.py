ac2ff614ff1dfd695906fe8be38def74f92bdd2b
yinnie <yinnie.yinnie@gmail.com>
2013-06-11 14:58:56 -0400

    moves working

--- tictactoe_f.py
+++ tictactoe_f.py
@@ -10,7 +10,7 @@ logging.basicConfig(filename=log_file, filemode="w+", level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 class Board(object):                                   
-    WINNING_BOARDS = [[[0,0], [0,1], [0,2]],     
+    winning_boards  = [[[0,0], [0,1], [0,2]],     
 						  [[1,0], [1,1], (1,2)],
 						  [[2,0], [2,1], [2,2]],
                           [[0,0], [1,0], [2,0]],
@@ -50,12 +50,13 @@ def show( list_of ):
 
 def play( a_board ):  
         if a_board.human:
-            move_result( a_board, human_move( a_board ),  "human")
-            a_board.human = False
-        if a_board.computer:
-           a_board = computer_move ( a_board)
-           a_board.computer = True
-           # a_board.human = True
+            next_turn = move_result( a_board, human_move( a_board ),  "human")
+            next_turn.human = False
+        elif a_board.computer:
+            next_turn = computer_move ( a_board)
+            # a_board.computer = True
+            next_turn.human = True
+        return next_turn
 
 def human_move( a_board ):
     print "place your move. enter row number of move"
@@ -75,22 +76,28 @@ def dumb_computer_move( a_board ):
 def move_result( a_board, the_move, player ):
     if check_valid_move(a_board, the_move[0], the_move[1]) :
        print player
-       update_board(a_board, player, the_move[0], the_move[1])
+       new_board = update_board(a_board, player, the_move[0], the_move[1])
     else:
-       move_result( a_board, human_move(a_board), player)
+       new_board =  move_result( a_board, human_move(a_board), player)
+
+    return new_board
 
 def update_board( a_board, player, row, col ):   
+    a_board = Board(a_board)
     if player == "human" :
        a_board.human_moves.append([row, col])
        a_board.BOARD[row][col] = 'x'
-       if check_winning(a_board.human_moves, a_board.WINNING_BOARDS):
+       if check_winning(a_board.human_moves, a_board.winning_boards):
           a_board.winner = "human"
-    else:
-       a_board.computer_moves.append([row, col])
-       a_board.BOARD[row][col] = 'o'
-       if check_winning(a_board.computer_moves, a_board.WINNING_BOARDS):
+    elif player == "computer":
+        a_board.computer_moves.append([row, col])
+        a_board.BOARD[row][col] = 'o'
+        if check_winning(a_board.computer_moves, a_board.winning_boards):
           a_board.winner = "computer"
-       
+    else:
+        raise Exception("Bad paramater in update_board: player")
+
+    return a_board   
     
 def check_winning( list_of_moves, list_of_winning_boards ):
     for element in list_of_winning_boards:     
@@ -114,12 +121,13 @@ def Minimax ( a_board ):
     return MAX ( a_board, 0)
 
 def MAX(a_board, depth):
+    print "depth is ", depth
+    print "Board is ", show(a_board.BOARD)
     board_temp = Board(a_board)
     if check_ending ( board_temp ):
        return leaf_value ( board_temp )
-    value =-2
-    the_right_move = board_temp 
 
+    value =-2
     for element in get_possible_boards (board_temp): 
         value_temp = MIN ( element, depth + 1 )       
         if value_temp > value:
@@ -148,16 +156,17 @@ def MIN ( a_board, depth ):
 def get_possible_boards ( a_board ):
     possible_boards = []
     for cell in get_empty_cells( a_board ):
-        new_board = Board(a_board)
-                      
         if a_board.human == True:
            #updating the newly created board with a mark in one of the empty cells
-           update_board ( new_board, "human", cell[0], cell[1])
+           new_board = update_board(a_board , "human", cell[0], cell[1])
         else:
-           update_board ( new_board, "computer", cell[0], cell[1])
+           new_board = update_board(a_board, "computer", cell[0], cell[1])
         possible_boards.append(new_board)
+
+    log.info("=========================================")
     for each_board in possible_boards:
         log.info( "possible board: {board}".format(board=each_board.BOARD))
+    log.info("=========================================")
     return possible_boards
     
 def get_empty_cells( a_board ):
@@ -178,10 +187,10 @@ def leaf_value ( a_board ):
        return 0
 
 def check_ending( a_board  ):   
-    if check_winning (a_board.human_moves, a_board.WINNING_BOARDS):
+    if check_winning (a_board.human_moves, a_board.winning_boards):
        a_board.winner = "human"
        return True
-    elif check_winning (a_board.computer_moves, a_board.WINNING_BOARDS):
+    elif check_winning (a_board.computer_moves, a_board.winning_boards):
        a_board.winner = "computer"
        return True
     elif a_board.BOARD.count == 0:
@@ -190,15 +199,15 @@ def check_ending( a_board  ):
     else: 
        return False
 
-board = Board()                      #board is the only globally exposed item.
-
-board_imaginary = Board()             #theoretical board for AI to do calculations
 
 def main():
+    board = Board()                      #board is the only globally exposed item.
+    board_imaginary = Board()             #theoretical board for AI to do calculations
     while True:
+          print "In While True"
           show( board.BOARD )
           if check_ending( board ) == False:
-              play( board )
+              board = play( board )
           elif board.winner == "human":
                  print "human won"
                  break
