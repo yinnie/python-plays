edcbda77cbc869d95f7351ff0806c0d601f39686
yinnie <yinnie.yinnie@gmail.com>
2013-06-14 12:24:41 -0400

    refactored some funcitons to be in-class fun

--- tictactoe_f.py
+++ tictactoe_f.py
@@ -9,7 +9,7 @@ log_file = realpath(join(dirname(__file__), "tictactoe.log"))
 logging.basicConfig(filename=log_file, filemode="w+", level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-class Board(object):                                   
+class Board (object):                                   
     winning_boards  = [[[0,0], [0,1], [0,2]],     
 						  [[1,0], [1,1], (1,2)],
 						  [[2,0], [2,1], [2,2]],
@@ -24,7 +24,7 @@ class Board(object):
         if board is None:
             self.human_moves = []                             
             self.computer_moves = []
-            self.turn = 0
+            self.turn = 0     #using this to switch turns 
             self.winner = None
             self.draw = False
         else:
@@ -34,7 +34,7 @@ class Board(object):
             self.winner = board.winner 
             self.draw = board.draw 
     @property
-    def BOARD(self):
+    def BOARD (self):
         BOARD = [[' ']*3 for x in range(0,3)]
         for (row, col) in self.human_moves: #(row col is a tuple)
             BOARD[row][col] = 'x'
@@ -44,18 +44,50 @@ class Board(object):
     def show(self):
         for element in self.BOARD:
             print "|".join(element)
-
-def play( a_board ):  
+    def check_ending ( self ):   
+        if check_winning ( self.human_moves, self.winning_boards ):
+           self.winner = "human"
+           return True
+        elif check_winning ( self.computer_moves, self.winning_boards ):
+           self.winner = "computer"
+           return True
+        else:
+            for element in self.BOARD:
+               for cell in element:
+                   if cell == ' ':
+                      return False
+            self.draw = True
+            return True
+    @property
+    def leaf_value ( self  ):                                 
+         if self.winner == "human":   
+            return -1
+         elif self.winner == "computer":
+            return 1
+         elif self.draw == True:
+            return 0
+         else:
+            print "this is not a terminal board"
+ 
+def play ( a_board, first_player, second_player ):  
+        next_turn = Board ( a_board )
         if a_board.turn == 0:
-            next_turn = move_result( a_board, human_move())
-            next_turn.turn = 1 
+            if first_player == "human":
+               next_turn = human_move_result( a_board, human_move())
+               next_turn.turn = 1 
+            elif first_player == "computer":
+               next_turn = computer_move ( a_board)
+               next_turn.turn = 1 
         elif a_board.turn == 1:
-            next_turn = computer_move ( a_board)
-            # a_board.computer = True
-            next_turn.turn = 0 
+            if second_player == "human":
+               next_turn = human_move_result( a_board, human_move())
+               next_turn.turn = 0 
+            elif second_player == "computer":
+               next_turn = computer_move ( a_board)
+               next_turn.turn = 0 
         return next_turn
 
-def human_move():
+def human_move ():
     print "place your move. enter row number of move (0 to 2)"
     row = int(raw_input())               
     print "enter column number of move( 0 to 2)"
@@ -66,15 +98,15 @@ def computer_move ( a_board ):
     print "player ", a_board.turn, "'s turn"
     return minimax ( a_board, 0 )
 
-def move_result( a_board, the_move ):
+def human_move_result ( a_board, the_move ):
     if check_valid_move(a_board, the_move[0], the_move[1]) :
        print "player ", a_board.turn, "'s turn"
        new_board = update_board(a_board, the_move[0], the_move[1])
     else:
-       new_board =  move_result( a_board, human_move())
+       new_board =  human_move_result( a_board, human_move())
     return new_board
 
-def update_board( a_board, row, col ):   
+def update_board ( a_board, row, col ):   
     a_board = Board(a_board)
     if a_board.turn == 0:
        a_board.human_moves.append([row, col])
@@ -90,7 +122,7 @@ def update_board( a_board, row, col ):
         raise Exception("Bad paramater in update_board: player")
     return a_board   
     
-def check_winning( list_of_moves, list_of_winning_boards ):
+def check_winning ( list_of_moves, list_of_winning_boards ):
     for element in list_of_winning_boards:     
         if element[0] in list_of_moves and\
            element[1] in list_of_moves and\
@@ -98,7 +130,7 @@ def check_winning( list_of_moves, list_of_winning_boards ):
            return True
     return False
 
-def check_valid_move( a_board, row, col ):
+def check_valid_move ( a_board, row, col ):
    if row > 2 or row < 0 or col > 2 or col < 0:
         print "invalid move. enter a number between 0 and 2"
         return False
@@ -110,15 +142,12 @@ def check_valid_move( a_board, row, col ):
 
 def minimax ( a_board, depth ):
     board_temp = Board(a_board)
-    if check_ending ( board_temp ):
-       return leaf_value ( board_temp )
+    if board_temp.check_ending():
+       return board_temp.leaf_value ()
     if board_temp.turn == 1:
        value = -2
     elif board_temp.turn == 0:
        value = 2
-    
-    the_right_move = Board()
-
     for element in get_possible_boards ( board_temp ): 
         value_temp = minimax ( element, depth + 1 )       
         if board_temp.turn == 1 and value_temp > value:
@@ -154,7 +183,7 @@ def get_possible_boards ( a_board ):
     log.info("=========================================")
     return possible_boards
     
-def get_empty_cells( a_board ):
+def get_empty_cells ( a_board ):
     empty_cells = []
     for row in range(0, 3): 
         for col  in range(0, 3):
@@ -162,40 +191,34 @@ def get_empty_cells( a_board ):
                empty_cells.append([row, col])
     return empty_cells
 
-def leaf_value ( a_board ):                                 
-    #the value of a certain board that we already know is ended
-    if a_board.winner == "human":   
-       return -1
-    elif a_board.winner == "computer":
-       return 1
-    elif a_board.draw == True:
-       return 0
+def set_players ( selection ):
+    if selection == 0:
+       first_player = "human"
+       second_player = "human"
+    elif selection == 1:
+       first_player = "human"
+       second_player = "computer"
+    elif selection == 2:
+       first_player = "computer"
+       second_player = "computer"
     else:
-       print "this is not a terminal board"
-
-def check_ending( a_board  ):   
-    if check_winning (a_board.human_moves, a_board.winning_boards):
-       a_board.winner = "human"
-       return True
-    elif check_winning (a_board.computer_moves, a_board.winning_boards):
-       a_board.winner = "computer"
-       return True
-    else:
-       for element in a_board.BOARD:
-           for cell in element:
-               if cell == ' ':
-                  return False
-       a_board.draw = True
-       return True
+       print "invalide selection. please enter 0, 1 or 2 to begin" 
+    return (first_player, second_player) 
 
 def main():
     board = Board()
+    print " menu "
+    print " enter 0 for human v.s. human"
+    print " enter 1 for human v.s. computer"
+    print " enter 2 for computer v.s. computer"
+    players = set_players (int(raw_input()))
+    print players[0]," v.s ",  players[1]
   #  board.human_moves=[[0,0],[1,1],[1,2]]  #debugging by making fuller boards
   #  board.computer_moves=[[0,2],[0,1],[1,0]]
     while True:
-          board.show( )
-          if check_ending( board ) == False:
-              board = play( board )
+          board.show()
+          if board.check_ending() == False:
+              board = play( board, players[0], players[1])
           elif board.winner == "human":
                  print "human won"
                  break
